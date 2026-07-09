678d599a1f59ee3edc7576aee07b892cfbee0b80
Mohammed Shaheer Imam <58761905+Shaheerimam@users.noreply.github.com>
2026-07-10 04:20:14 +0600

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+ 
+typedef long long ll;
+using namespace std;
+void solve(){
+    ll n;
+    cin >> n;
+    if(n%3==0){
+        cout <<"Second
+";
+    }else{
+        cout <<"First
+";
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
