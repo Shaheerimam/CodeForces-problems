d8bbfb8504d9bcb4089f4578d2d828dcf67e876b
Mohammed Shaheer Imam <58761905+Shaheerimam@users.noreply.github.com>
2026-07-10 04:19:30 +0600

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+ 
+typedef long long ll;
+using namespace std;
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin >> v[i];
+    }
+    sort(v.begin(),v.end());
+    cout <<(v[n-1]-v[0])+1<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while(tc--){
+        solve();
+    }
+}
