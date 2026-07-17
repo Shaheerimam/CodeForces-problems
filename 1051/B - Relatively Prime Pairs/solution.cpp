1dcf6ad6fdd5e8df57715dda718bf5a1f3b1c51b
Mohammed Shaheer Imam <58761905+Shaheerimam@users.noreply.github.com>
2026-07-17 21:36:06 +0600

    Add Relatively Prime Pairs [B] from Codeforces

---
+++ 1051/B - Relatively Prime Pairs/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+/*
+|-------------------|
+|Solved by,         |
+|.  SHAHEER IMAM.   |
+|-------------------|
+*/
+typedef long long ll;
+using namespace std;
+void solve(){
+    
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll l,r;
+    cin >> l >>r;
+    cout << "YES
+";
+    l++;
+    for(;l<=r;l+=2){
+        cout << l-1 <<" "<<l<<endl;
+    }
+}
